6fbced2ef2c71e2abb3ce77366f546c80ad6ab49
el7ossen <husaindwan0@gmail.com>
2025-02-06 20:39:45 +0300

    made it work like apache

--- main.go
+++ main.go
@@ -5,16 +5,13 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Welcome to my Go Web Server!</h1>")
-}
-
 func main() {
-	http.HandleFunc("/", handler) // Set route for the homepage
+	fs := http.FileServer(http.Dir(".")) // Serve files from "static" directory
+	http.Handle("/", fs)
 
-	fmt.Println("Starting server on :8080...")
-	err := http.ListenAndServe(":8080", nil) // Listen on port 8080
+	fmt.Println("Server running on :8080...")
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		fmt.Println("Error starting server:", err)
+		fmt.Println("Error:", err)
 	}
 }
