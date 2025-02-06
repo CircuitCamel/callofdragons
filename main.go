c4d6fb8708d3aed03c6acad3d819ddcd032ac6ea
el7ossen <husaindwan0@gmail.com>
2025-02-06 20:42:27 +0300

    changed port to 5098

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ func main() {
 	fs := http.FileServer(http.Dir(".")) // Serve files from "static" directory
 	http.Handle("/", fs)
 
-	fmt.Println("Server running on :8080...")
-	err := http.ListenAndServe(":8080", nil)
+	fmt.Println("Server running on :5098...")
+	err := http.ListenAndServe(":5098", nil)
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
